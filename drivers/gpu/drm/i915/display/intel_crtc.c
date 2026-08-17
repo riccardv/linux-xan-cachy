6f9a6f8cb44a76adda60dc2a94a62753bf36dc27
Riccardo Varotto <riccardo.varotto@keysight.com>
2026-08-17 12:21:58 +0200

    [32m7.1-xan-infinity[m Revert "[32m7.1-xan-cachy[m i915-rt"
    This reverts commit 72566b881fa6272f5ce2b92df0514dc4f8cbabbe.

--- drivers/gpu/drm/i915/display/intel_crtc.c
+++ drivers/gpu/drm/i915/display/intel_crtc.c
@@ -630,8 +630,7 @@ void intel_pipe_update_start(struct intel_atomic_state *state,
 	 */
 	intel_psr_wait_for_idle_locked(new_crtc_state);
 
-	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
-		local_irq_disable();
+	local_irq_disable();
 
 	crtc->debug.min_vbl = evade.min;
 	crtc->debug.max_vbl = evade.max;
@@ -649,8 +648,7 @@ void intel_pipe_update_start(struct intel_atomic_state *state,
 	return;
 
 irq_disable:
-	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
-		local_irq_disable();
+	local_irq_disable();
 }
 
 #if IS_ENABLED(CONFIG_DRM_I915_DEBUG_VBLANK_EVADE)
@@ -797,8 +795,7 @@ void intel_pipe_update_end(struct intel_atomic_state *state,
 	     !new_crtc_state->vrr.enable))
 		intel_vrr_send_push(NULL, new_crtc_state);
 
-	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
-		local_irq_enable();
+	local_irq_enable();
 
 	if (intel_parent_vgpu_active(display))
 		goto out;
